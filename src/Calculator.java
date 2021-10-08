bca96f2ead4b6b8565dcbcefbb0d35787070990a
florian-lirzer <s50759@edu.campus02.at>
2021-10-08 19:58:46 +0200

    Variablen deklariert

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,3 +1,14 @@
 public class Calculator {
-
+    public static void main(String[] args){
+        int i = 1;
+        System.out.println(i);
+        double d = 1000000000;
+        System.out.println(d);
+        boolean b = true;
+        System.out.println(b);
+        char c = 'A';
+        System.out.println(c);
+        String s = "Hallo";
+        System.out.println(s);
+    }
 }
