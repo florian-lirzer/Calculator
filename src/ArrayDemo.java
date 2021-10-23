9ade5664ef87b78200a78d5a53d724c964a39767
florian-lirzer <s50759@edu.campus02.at>
2021-10-23 10:42:49 +0200

    neues Kommentar

--- src/ArrayDemo.java
+++ src/ArrayDemo.java
@@ -4,7 +4,7 @@ public class ArrayDemo
     {
         double[] articles = new double[5];
         double sum = 0.0;
-
+        //neues Kommentar
         articles[0] = 2.4;
         articles[1] = 2.0;
         articles[2] = 2.9;
@@ -16,5 +16,17 @@ public class ArrayDemo
 
         System.out.println(sum);
 
+        int[] array1 = {3, 4, 5};
+        int[] array2 = {7, 1, 3};
+
+        for(int i = 0; i < array1.length && i < array2.length; i++)
+        {
+            System.out.println("+: " + (array1[i] + array2[i]));
+        }
+
+        for(int i = 0; i < array1.length && i < array2.length; i++)
+        {
+            System.out.println("*: " + (array1[i] * array2[i]));
+        }
     }
 }
